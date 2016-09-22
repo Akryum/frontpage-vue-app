59e5736255fa421c51a5f29ba600e176d3edc06d
Guillaume Chau <guillaume.b.chau@gmail.com>
2016-09-22 19:07:51 +0200

    Removed unused css, updated vue-apollo

--- src/App.vue
+++ src/App.vue
@@ -29,33 +29,9 @@ ul {
   padding-left: 20px;
 }
 
-ul li {
-  list-style: none;
-}
-
 ul li span {
   color: #999;
   margin-right: 10px;
-}
-
-.app {
-  text-align: center;
-}
-
-.app-logo {
-  animation: App-logo-spin infinite 20s linear;
-  height: 80px;
-}
-
-.app-header {
-  background-color: #222;
-  height: 150px;
-  padding: 20px;
-  color: white;
-}
-
-.app-intro {
-  font-size: large;
 }
 
 @keyframes App-logo-spin {
